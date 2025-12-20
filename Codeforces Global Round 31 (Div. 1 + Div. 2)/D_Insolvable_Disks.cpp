83b6a11920fbc32c08b77f199dd95ffdfae6160a
BurhanAkkus <burhan.akkus@outlook.com>
2025-12-20 05:21:59 +0100

    solved D

--- Codeforces Global Round 31 (Div. 1 + Div. 2)/D_Insolvable_Disks.cpp
+++ Codeforces Global Round 31 (Div. 1 + Div. 2)/D_Insolvable_Disks.cpp
@@ -4,49 +4,45 @@ using namespace std;
 
 #define ll long long 
 #define INFINITY 1e9+1
-void solve(int t){
+
+void solve(){
     ll n ;
     cin >> n;
-    vector<ll> x(n,0);
-    vector<pair<ll,ll>> araliklar(n-1);
+    vector<ll> x(n+5,0);
     vector<pair<ll,ll>> kisitlamalar(n,{0,INFINITY});
     for(ll i = 0; i < n; i++){
         cin >> x[i];
     }
     for(ll i = 0; i < n-1; i++){
-        araliklar[i] = {i,x[i+1] - x[i]};
         kisitlamalar[i] = {0,min(kisitlamalar[i].second, x[i+1] - x[i])};
         kisitlamalar[i+1] = {0,min(kisitlamalar[i+1].second, x[i+1] - x[i])};
     }
-    sort(araliklar.begin(),araliklar.end(),[](pair<ll,ll> a, pair<ll,ll> b){
-        return a.second < b.second;
-    });
     ll ans = 0;
     for(ll i = 0 ; i < n-1;i++){
-        pair<ll,ll> aralik = araliklar[i];
-        pair<ll,ll> firstKisitlama = kisitlamalar[araliklar[i].first];
-        pair<ll,ll> secondKisitlama = kisitlamalar[aralik.first + 1];
-        if(aralik.second > firstKisitlama.first + secondKisitlama.first && aralik.second < firstKisitlama.second + secondKisitlama.second){
-            ans ++;
-            ll yeniFirstMin = max(firstKisitlama.first, aralik.second - secondKisitlama.second);
-            ll yeniFirstMax= min(firstKisitlama.second, aralik.second - secondKisitlama.first);
-            ll yeniSecondMin = max(secondKisitlama.first, aralik.second - firstKisitlama.second);
-            ll yeniSecondMax = min(secondKisitlama.second, aralik.second - firstKisitlama.first);
-            kisitlamalar[aralik.first] = {yeniFirstMin,yeniFirstMax};
-            kisitlamalar[aralik.first + 1] = {yeniSecondMin,yeniSecondMax};
+        pair<ll,ll> tasimaliKisitlama = kisitlamalar[i];
+        ll interval = x[i+1] - x[i];
+        ll j = 1;
+        for(; i+j < n; j++){
+            // tek sayida intervalse
+            // yani ciftinci sayida sayiysa
+            if(j&1){
+                tasimaliKisitlama.first = max(tasimaliKisitlama.first, interval - kisitlamalar[i+j].second);
+                tasimaliKisitlama.second = min(tasimaliKisitlama.second, interval);
+                interval -= x[i+j+1] - x[i+j];
+            }
+            // ciftinci sayida intervalse
+            // yani tekinci sayida sayiysa
+            else{
+                tasimaliKisitlama.first = max(tasimaliKisitlama.first, interval);
+                tasimaliKisitlama.second = min(tasimaliKisitlama.second, interval + kisitlamalar[i+j].second);
+                interval += x[i+j+1] - x[i+j];
+            }
+            if(tasimaliKisitlama.first >= tasimaliKisitlama.second) break;
         }
+        ans += j - 1;
+        i+=j - 1;
     }
-    if(t == 10000 - 1701){
-        for(ll i = 0; i < n; i++){
-        cout << x[i] << " ";
-        if(i%20 == 19){
-            cout << "\n";
-        }
-    }
-    cout << "\n";
-    }
-    if(t < 4)
-    cout<< ans << "\n";
+    cout << ans << "\n";
 
 }
 
@@ -57,6 +53,6 @@ int main(){
     long long t;
     cin >> t;
     while(t--){
-        solve(t);
+        solve();
     }
 }
